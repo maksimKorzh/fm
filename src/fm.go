aee3b109ecb215acf3b16514bd5dd5db497297bb
Code Monkey King <freesoft.for.people@gmail.com>
2023-07-15 12:17:10 +0300

    added navigation through dirs

--- src/fm.go
+++ src/fm.go
@@ -1,11 +1,14 @@
 package main
 
-import "os"
-import "fmt"
-//import "strings"
-import "strconv"
-import "github.com/nsf/termbox-go"
-import "github.com/mattn/go-runewidth"
+import (
+  "os"
+  "fmt"
+  "runtime"
+  "strings"
+  "strconv"
+  "github.com/nsf/termbox-go"
+  "github.com/mattn/go-runewidth"
+)
 
 var COLS, ROWS int
 var LEFT = 0
@@ -20,7 +23,7 @@ type Panel struct {
 
 var panels = []Panel {
   {
-    path: "/home/cmk",
+    path: "/home/cmk/Desktop",
     panel: []map[string]interface{}{},
     current_row: 0,
     row_offset: 0,
@@ -65,14 +68,18 @@ func load_panel(active int) {
   panel, err := dir.Readdir(-1);
   defer dir.Close(); if err != nil { return }
   load_panel := []map[string]interface{}{}
-  load_panel = append(load_panel, map[string]interface{}{"name": ".."})
+  load_panel = append(load_panel, map[string]interface{}{"name": "..", "dir": true})
   for row := 0; row < len(panel); row++ {
+    exec := false
+    if runtime.GOOS == "windows" {
+      exec = strings.Contains(panel[row].Name(), ".exe")
+    } else if runtime.GOOS == "linux" { exec = panel[row].Mode()&0111 != 0 }
     info := map[string]interface{}{
       "name": panel[row].Name(),
       "size": strconv.FormatInt(panel[row].Size(), 10),
       "date": panel[row].ModTime().Format("2006-01-02"),
       "dir": panel[row].IsDir(),
-      "exec": panel[row].Mode().Perm()&0111,
+      "exec": exec,
       "inserted": false,
     }; load_panel = append(load_panel, info)
   };panels[active].panel = load_panel
@@ -88,14 +95,19 @@ func display_panel(offset, active int) {
   if current_row < row_offset { row_offset = current_row }
   if current_row >= row_offset + ROWS-2 { row_offset = current_row - ROWS+2+1 }
   panels[active].row_offset = row_offset
+  print_message(2, 0, termbox.ColorBlue | termbox.AttrBold, termbox.ColorDefault, panels[LEFT].path)
+  print_message(2+int(COLS/2), 0, termbox.ColorBlue | termbox.AttrBold, termbox.ColorDefault, panels[RIGHT].path)
   for row := 0; row < ROWS-2; row++ {
     buffer_row := row + row_offset
     if buffer_row < len(panel) {
       if row < ROWS-2 {
+        fgcolor := termbox.ColorWhite;
+        if panel[buffer_row]["dir"] == true { fgcolor = termbox.ColorYellow | termbox.AttrBold
+        } else if panel[buffer_row]["exec"] == true { fgcolor = termbox.ColorGreen | termbox.AttrBold }
         if active == LEFT {
-          print_message(1, row+1, termbox.ColorWhite, termbox.ColorBlue, panel[buffer_row]["name"].(string))
+          print_message(1, row+1, fgcolor, termbox.ColorDefault, panel[buffer_row]["name"].(string))
         } else {
-          print_message(1+offset, row+1, termbox.ColorWhite, termbox.ColorBlue, panel[buffer_row]["name"].(string))
+          print_message(1+offset, row+1, fgcolor, termbox.ColorDefault, panel[buffer_row]["name"].(string))
         }
       }
     }
@@ -106,7 +118,7 @@ func display_panel(offset, active int) {
       } else { col_from = int(COLS/2)+1; col_to = COLS-1 }
       for col := col_from; col < col_to; col++ {
         current_cell := termbox.GetCell(col, row+1)
-        termbox.SetCell(col, row+1, current_cell.Ch, termbox.ColorDefault, termbox.ColorYellow)
+        termbox.SetCell(col, row+1, current_cell.Ch, current_cell.Fg, termbox.ColorBlue)
       }
     }
   }
@@ -135,15 +147,24 @@ func get_event() termbox.Event {
 func process_keypress() {
   event := get_event()
   current_row := &panels[active_panel].current_row
-  current_panel := &panels[active_panel].panel
+  current_panel := panels[active_panel].panel
   switch event.Key {
     case termbox.KeyEsc: termbox.Close(); os.Exit(0)
-    case termbox.KeyArrowDown: if *current_row < len(*current_panel)-1 { *current_row++ }
+    case termbox.KeyArrowDown: if *current_row < len(current_panel)-1 { *current_row++ }
     case termbox.KeyArrowUp: if *current_row > 0 { *current_row-- }
     case termbox.KeyArrowRight: active_panel = RIGHT
     case termbox.KeyArrowLeft: active_panel = LEFT
     case termbox.KeyEnter: {
-
+      split_ch := "/"
+      if current_panel[*current_row]["name"] == ".." {
+        split_path := strings.Split(panels[active_panel].path, split_ch)
+        panels[active_panel].path = strings.Join(split_path[:len(split_path)-1], split_ch)
+        if panels[active_panel].path == "" { panels[active_panel].path = "/"}
+        load_panel(active_panel)
+      } else if current_panel[*current_row]["dir"] == true {
+        panels[active_panel].path += split_ch + current_panel[*current_row]["name"].(string)
+        load_panel(active_panel); *current_row = 0
+      }
     }
   }
 }
@@ -155,7 +176,7 @@ func run_file_manager() {
   load_panel(RIGHT)
   for {
     COLS, ROWS = termbox.Size()
-    termbox.Clear(termbox.ColorWhite, termbox.ColorBlue)
+    termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
     display_panels()
     display_files()
     termbox.Flush()
